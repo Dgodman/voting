b944cbd57c6fd195a6da766f88465a2f80519d52
Dgodman <dgodman@gmail.com>
2016-07-27 13:56:36 -0400

    postgres to sqlite

--- voting_project/settings.py
+++ voting_project/settings.py
@@ -75,13 +75,19 @@ WSGI_APPLICATION = 'voting_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+'''
+'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'votedb',
+    'USER': 'lemonhead',
+    'PASSWORD': 'blahshoe1'
+}
+'''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'votedb',
-        'USER': 'lemonhead',
-        'PASSWORD': 'blahshoe1'
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 
